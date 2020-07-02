c85bb954d81abae31bb51475feb91e13b026ae0d
Sunrise <sunrise4ws@gmail.com>
2020-07-02 18:40:15 -0500

    layout complete and some key value to display functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,22 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
+
+let displayText = document.getElementById("display-nums");
+let keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  if (key.hasAttribute("data-value")) {
+    key.addEventListener("click", (e) => {
+      let text = document.createTextNode(`${key.getAttribute("data-value")}`);
+      displayText.appendChild(text);
+
+      console.log(e);
+    });
+  }
+});
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     alert(event.target);
+//   });
+// });
