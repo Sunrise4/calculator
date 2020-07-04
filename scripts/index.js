fe83a3983ea4d6f1e6e5916ec0ceb1cd682f50c4
Sunrise <sunrise4ws@gmail.com>
2020-07-03 19:17:35 -0500

    added more functionality with keys and better workspace

--- scripts/index.js
+++ scripts/index.js
@@ -17,17 +17,45 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
-
-let displayText = document.getElementById("display-nums");
-let keys = document.querySelectorAll(".key");
+let dec = 0;
+let text = '';
+const displayText = document.getElementById('display-nums');
+const keys = document.querySelectorAll('.key');
 keys.forEach((key) => {
-  if (key.hasAttribute("data-value")) {
-    key.addEventListener("click", (e) => {
-      let text = document.createTextNode(`${key.getAttribute("data-value")}`);
+  if (key.hasAttribute('data-value')) {
+    key.addEventListener('click', () => {
+      text = document.createTextNode(
+        `${key.getAttribute('data-value')}`,
+      );
       displayText.appendChild(text);
-
-      console.log(e);
     });
+  }
+  if (key.hasAttribute('data-op')) {
+    key.addEventListener('click', () => {
+      text = document.createTextNode(` ${key.textContent} `);
+      displayText.appendChild(text);
+      dec = 0;
+    });
+  }
+  if (key.hasAttribute('data-clear')) {
+    key.addEventListener('click', () => {
+      displayText.textContent = '';
+      dec = 0;
+    });
+  }
+  if (key.hasAttribute('data-dec')) {
+    key.addEventListener('click', () => {
+      if (dec === 0) {
+        text = document.createTextNode(
+          `${key.getAttribute('data-dec')}`,
+        );
+        displayText.appendChild(text);
+      }
+      dec = 1;
+    });
+  }
+  if (key.hasAttribute('data-equals')) {
+    key.addEventListener('click', () => {});
   }
 });
 
