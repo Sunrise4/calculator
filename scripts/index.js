867919b9e5d2f01887d59ee9c429e07c3bd752be
Sunrise <sunrise4ws@gmail.com>
2020-07-05 19:22:12 -0500

    all operations functional

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+let equation = [];
+let last = '';
+let dec = 0;
+let text = '';
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -14,37 +19,64 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-  return operator(num1, num2);
+function operate(eqArr) {
+  let solution = 0;
+  let num1 = 0;
+  let op = '';
+  let num2 = 0;
+
+  while (eqArr.length > 2) {
+    num1 = parseFloat(eqArr.shift());
+    op = eqArr.shift();
+    num2 = parseFloat(eqArr.shift());
+    solution = window[op](num1, num2);
+    eqArr.unshift(solution.toString());
+  }
+
+  return solution;
 }
-let equation = [];
-let dec = 0;
-let text = '';
+
 const displayText = document.getElementById('display-nums');
 const keys = document.querySelectorAll('.key');
 keys.forEach((key) => {
+  // data-value 0-9
   if (key.hasAttribute('data-value')) {
     key.addEventListener('click', () => {
       text = document.createTextNode(
         `${key.getAttribute('data-value')}`,
       );
       displayText.appendChild(text);
+
+      if (last === 'val' || last === 'dec') {
+        equation[equation.length - 1] += key.getAttribute(
+          'data-value',
+        );
+      } else {
+        equation.push(key.getAttribute('data-value'));
+      }
+      last = 'val';
     });
   }
+  // data-op +-*/
   if (key.hasAttribute('data-op')) {
     key.addEventListener('click', () => {
       text = document.createTextNode(` ${key.textContent} `);
       displayText.appendChild(text);
-      equation.append(key.getAttribute('data-op'));
+      equation.push(key.getAttribute('data-op'));
+      last = 'op';
       dec = 0;
     });
   }
+  // data-clear
   if (key.hasAttribute('data-clear')) {
     key.addEventListener('click', () => {
       displayText.textContent = '';
+      equation = [];
+      last = '';
       dec = 0;
     });
   }
+  // data-dec .
   if (key.hasAttribute('data-dec')) {
     key.addEventListener('click', () => {
       if (dec === 0) {
@@ -52,17 +84,25 @@ keys.forEach((key) => {
           `${key.getAttribute('data-dec')}`,
         );
         displayText.appendChild(text);
+
+        if (last === 'val') {
+          equation[equation.length - 1] += key.getAttribute(
+            'data-dec',
+          );
+        } else {
+          equation.push(key.getAttribute('data-dec'));
+        }
+        last = 'dec';
       }
+
       dec = 1;
     });
   }
+  // data-equals =
   if (key.hasAttribute('data-equals')) {
-    key.addEventListener('click', () => {});
+    key.addEventListener('click', () => {
+      const solution = operate(equation);
+      displayText.textContent = solution;
+    });
   }
 });
-
-// buttons.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     alert(event.target);
-//   });
-// });
